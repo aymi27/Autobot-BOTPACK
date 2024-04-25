591cfa190d854101a68c3ca8bc8fae723c4c736b
aymi27 <166910421+aymi27@users.noreply.github.com>
2024-04-25 17:45:07 +0800

    Update prefix.js

--- script/prefix.js
+++ script/prefix.js
@@ -32,10 +32,9 @@ module.exports.handleEvent = function ({ api, event, client, __GLOBAL }) {
                                                                 {
                                                                                 body: `𝖧𝖤𝖸! 𝖳𝖧𝖨𝖲 𝖨𝖲 𝖬𝖸 𝖯𝖱𝖤𝖥𝖨𝖷 :   𓆩 ${global.config.PREFIX} 𓆪 \n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
-➥ ${global.config.PREFIX}𝗁𝖾𝗅𝗉 [𝗇𝗎𝗆𝖻𝖾𝗋 𝗈𝖿 𝗉𝖺𝗀𝖾] ► 𝗌𝖾𝖾 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝗌
-➥ ${global.config.PREFIX}𝗌𝗂𝗆 [𝗆𝖾𝗌𝗌𝖺𝗀𝖾] ► 𝗍𝖺𝗅𝗄 𝗍𝗈 𝖻𝗈𝗍
-➥ ${global.config.PREFIX}𝖼𝖺𝗅𝗅𝖺𝖽 [𝗆𝖾𝗌𝗌𝖺𝗀𝖾] ► 𝗋𝖾𝗉𝗈𝗋𝗍 𝖺𝗇𝗒 𝗉𝗋𝗈𝖻𝗅𝖾𝗆 𝖾𝗇𝖼𝗈𝗎𝗇𝗍𝖾𝗋𝖾𝖽
-➥ ${global.config.PREFIX}𝗁𝖾𝗅𝗉 [𝖼𝗈𝗆𝗆𝖺𝗇𝖽] ► 𝗂𝗇𝖿𝗈𝗋𝗆𝖺𝗍𝗂𝗈𝗇 𝖺𝗇𝖽 𝗎𝗌𝖺𝗀𝖾 𝗈𝖿 𝖼𝗈𝗆𝗆𝖺𝗇𝖽\n\n𝖧𝖺𝗏𝖾 fun 𝗎𝗌𝗂𝗇𝗀 𝗂𝗍 𝖾𝗇𝗃𝗈𝗒!❤️\n𝖡𝗈𝗍 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋: ${global.config.OWNERLINK} `,
+〘🤖〙 ${global.config.PREFIX}𝗁𝖾𝗅𝗉 [𝗇𝗎𝗆𝖻𝖾𝗋 𝗈𝖿 𝗉𝖺𝗀𝖾] ► 𝗌𝖾𝖾 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝗌
+〘🤖〙 ${global.config.PREFIX}𝗌𝗂𝗆 [𝗆𝖾𝗌𝗌𝖺𝗀𝖾] ► 𝗍𝖺𝗅𝗄 𝗍𝗈 𝖻𝗈?
+〘🤖〙 ${global.config.PREFIX}𝗁𝖾𝗅𝗉 [𝖼𝗈𝗆𝗆𝖺𝗇𝖽] ► 𝗂𝗇𝖿𝗈𝗋𝗆𝖺𝗍𝗂𝗈𝗇 𝖺𝗇𝖽 𝗎𝗌𝖺𝗀𝖾 𝗈𝖿 𝖼𝗈𝗆𝗆𝖺𝗇𝖽\n\n𝖧𝖺𝗏𝖾 fun 𝗎𝗌𝗂𝗇𝗀 𝗂𝗍 𝖾𝗇𝗃𝗈𝗒!❤️\n𝖡𝗈𝗍 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋: ${global.config.OWNERLINK} `,
                                                                                 attachment: fs.createReadStream(
                                                                                                 __dirname + `/noprefix/prefix.gif`
                                                                                 ),
